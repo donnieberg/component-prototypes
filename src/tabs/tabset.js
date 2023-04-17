1b46057ce978f5bb54eda9e9280300a44d08a456
Doneille Berg <d.berg@dberg-ltm.internal.salesforce.com>
2023-04-17 14:58:08 -0700

    Make tabset functional

--- src/tabs/tabset.js
+++ src/tabs/tabset.js
@@ -1,44 +1,90 @@
 import classNames from 'classnames';
+import { useState } from 'react';
 import OverflowTabMenu from './overflowTabMenu.js';
+import utils from '../utils.js'
 
 function Tabs() {
+    const tabData = [
+        { index: 0, id: '0_tab', title: 'Accounts', content: 'content for accounts' },
+        { index: 1, id: '1_tab', title: 'Approvals', content: 'content for Approvals' },
+        { index: 2, id: '2_tab', title: 'Leads', content: 'content for Leads' },
+        { index: 3, id: '3_tab', title: 'Opportunities', content: 'content for opportunities' },
+        { index: 4, id: '4_tab', title: 'Contacts', content: 'content for contacts' },
+        { index: 5, id: '5_tab', title: 'Reports', content: 'content for reports' },
+        { index: 6, id: '6_tab', title: 'Favorites', content: 'content for Favorites' },
+    ];
+
+    let [currentTab, setcurrentTab] = useState(tabData[0]);
+    let [currentData, setcurrentData] = useState(tabData);
+
+    const handleKeyDown = (index) => {
+        return (e) => {
+
+            if(e.keyCode == utils.keys.right) {
+                const nextTab = currentData[index + 1];
+                const nextEl = document.getElementById(nextTab.id);
+                if(nextEl) {
+                    nextEl.focus()
+                    setcurrentTab(currentTab = nextTab)
+                }
+            } else if (e.keyCode == utils.keys.left) {
+                const prevTab = currentData[index - 1];
+                const prevEl = prevTab && document.getElementById(prevTab.id);
+                if(prevEl) {
+                    prevEl.focus();
+                    setcurrentTab(currentTab = prevTab)
+                }
+            }
+        }
+    };
+
+    const tabTitles = currentData.slice(0, 4).map( data => {
+        return (
+            <li key={data.id} 
+                className={classNames("slds-tabs_default__item", 
+                    { 
+                        'slds-is-active': currentTab.index == data.index
+                    })}
+                title="Item One" 
+                role="presentation">
+                <a className="slds-tabs_default__link" href="#" 
+                    role="tab" 
+                    tabIndex="0" 
+                    aria-selected="true" 
+                    aria-controls={`tab-default-${data.index}`}
+                    onKeyDown={handleKeyDown(data.index)}
+                    id={data.id}>
+                    {data.title}
+                </a>
+            </li>
+        )
+    })
+
+    const tabContent = currentData.slice(0, 4).map(data => {
+        return (
+            <div key={data.id} 
+                id={data.id} 
+                className={classNames("slds-tabs_default__content", 
+                    { 
+                        'slds-show': currentTab.index == data.index,
+                        'slds-hide': currentTab.index != data.index, 
+                    })}
+                role="tabpanel" 
+                aria-labelledby={data.id}>
+                <h2>{data.content}</h2>
+            </div>
+        )
+    });
+
     return (
         <div className="slds-tabs_default">
             <ul className="slds-tabs_default__nav" role="tablist">
-                <li className="slds-tabs_default__item slds-is-active" title="Item One" role="presentation">
-                    <a className="slds-tabs_default__link" href="#" role="tab" tabIndex="0" aria-selected="true" aria-controls="tab-default-1" id="tab-default-1__item">Item One</a>
-                </li>
-                <li className="slds-tabs_default__item" title="Item Two" role="presentation">
-                    <a className="slds-tabs_default__link" href="#" role="tab" tabIndex="-1" aria-selected="false" aria-controls="tab-default-2" id="tab-default-2__item">Item Two</a>
-                </li>
-                <li className="slds-tabs_default__item" title="Item Three" role="presentation">
-                    <a className="slds-tabs_default__link" href="#" role="tab" tabIndex="-1" aria-selected="false" aria-controls="tab-default-3" id="tab-default-3__item">Item Three</a>
-                </li>
-                <li className="slds-tabs_default__item" title="Item Four" role="presentation">
-                    <a className="slds-tabs_default__link" href="#" role="tab" tabIndex="-1" aria-selected="false" aria-controls="tab-default-4" id="tab-default-4__item">Item Four</a>
-                </li>
-                <li className="slds-tabs_default__item" title="Item Five" role="presentation">
-                    <a className="slds-tabs_default__link" href="#" role="tab" tabIndex="-1" aria-selected="false" aria-controls="tab-default-5" id="tab-default-5__item">Item Five</a>
-                </li>
+                {tabTitles}
                 <li className="slds-tabs_default__item slds-tabs_default__overflow-button" title="More Tabs" role="presentation">
                     <OverflowTabMenu />
                 </li>
             </ul>
-            <div id="tab-default-1" className="slds-tabs_default__content slds-show" role="tabpanel" aria-labelledby="tab-default-1__item">
-                <h2>Item One Content</h2>
-            </div>
-            <div id="tab-default-2" className="slds-tabs_default__content slds-hide" role="tabpanel" aria-labelledby="tab-default-2__item">
-                <h2>Item Two Content</h2>
-            </div>
-            <div id="tab-default-3" className="slds-tabs_default__content slds-hide" role="tabpanel" aria-labelledby="tab-default-3__item">
-                <h2>Item Three Content</h2>
-            </div>
-            <div id="tab-default-4" className="slds-tabs_default__content slds-hide" role="tabpanel" aria-labelledby="tab-default-4__item">
-                <h2>Item Four Content</h2>
-            </div>
-            <div id="tab-default-5" className="slds-tabs_default__content slds-hide" role="tabpanel" aria-labelledby="tab-default-5__item">
-                <h2>Item Five Content</h2>
-            </div>
+            {tabContent}
         </div>
     );
 }
---
+++ src/utils.js
@@ -0,0 +1,12 @@
+const utils = {
+    keys: {
+        left: 37,
+        up: 38,
+        right: 39,
+        down: 40
+    }
+};
+
+export default utils;
+
+
