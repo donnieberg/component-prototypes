324649edb8d339d0b168f7834f63cf5ceeb02d5e
Adam Rodenbeck <arodenbeck@salesforce.com>
2023-04-27 15:47:45 -0400

    Added the event handler for hiding and displaing the error message. Attempted to update aria-describedby on the input when the event fires, but still having trouble with this.

--- src/errorMessaging/errorMessaging.js
+++ src/errorMessaging/errorMessaging.js
@@ -8,6 +8,21 @@ const ErrorMessaging = ({ ariaLive }) => {
             return 'alert';
         }
     };
+    let describedbyId = "";
+    const describedbyValue = () => {
+        return describedbyId;
+    };  // End describedbyValue function
+    const handleErrorOnBlur = (e) => {
+        let formInput1 = document.getElementById("form-input-1");
+        let nameInput = document.getElementById("name-input-1");
+        let nameError = document.getElementById("input-error-1");
+        if(e.target.value.length < 1) {
+            formInput1.classList.add("slds-has-error");
+            e.target.setAttribute("aria-invalid", true);
+            describedbyId = "input-error-1";
+            nameError.style.display = "block";
+        }  // End if the input is empty
+    };  // End handleOnBlur event handler
 
     return (
         <div className="pam">
@@ -18,15 +33,15 @@ const ErrorMessaging = ({ ariaLive }) => {
                 </label>
                 <div className="slds-form-element__control">
                     <input 
-                        aria-describedby="input-error-1" 
+                        aria-describedby={describedbyValue()}
                         className="slds-input" 
                         id="name-input-1" 
                         required={true} 
-                        role={renderRole()}
                         type="text" 
+                        onBlur={handleErrorOnBlur}
                     />
                 </div>
-                <div className="slds-form-element__help" id="input-error-1" role={renderRole()}>Complete this field</div>
+                <div className="slds-form-element__help" id="input-error-1" role={renderRole()} style={{display: "none"}}>Complete this field</div>
             </div>
             <div className="slds-form-element" id="form-input-2">
                 <label className="slds-form-element__label" htmlFor="phone-input-1">
@@ -34,7 +49,6 @@ const ErrorMessaging = ({ ariaLive }) => {
                 </label>
                 <div className="slds-form-element__control">
                     <input 
-                        aria-describedby={ariaLive == 'none' ? "input-error-1" : null} 
                         className="slds-input" 
                         id="phone-input-1" 
                         role={renderRole()}
