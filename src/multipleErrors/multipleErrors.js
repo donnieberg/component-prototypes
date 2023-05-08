a2e21f900e77c548b465ef8d4e657cf889d0428b
Adam Rodenbeck <arodenbeck@salesforce.com>
2023-05-08 17:33:03 -0400

    Started work on iterating over the map to display the fields.

--- src/multipleErrors/multipleErrors.js
+++ src/multipleErrors/multipleErrors.js
@@ -1,38 +1,79 @@
-const multipleErrors = ({errorStyle}) => {
-  const [state, setState] = useState({
-    fName: "",
-    lName: "",
-    emailAddress: "",
-    mobile: "",
-    company: ""
-  });  // End setup of useState
+import { useState, useRef } from ‘react’;
+const MultipleErrors = ({errorStyle}) => {
+  const fnameRef = useRef(null);
+  const [state, setState] = useState(new Map());
+  setState(new Map(state.set('fname', {
+    inputName: 'fname',
+    label: 'First name',
+    inputRef: useRef(null),
+    inputValue: ''
+  })));
+  setState(new Map(state.set('lname', {
+    inputName: 'lname',
+    label: 'Last name',
+    inputRef: useRef(null),
+    inputValue: ''
+  })));
+  setState(new Map(state.set('email', {
+    inputName: 'email',
+    label: 'Email address',
+    inputRef: useRef(null),
+    inputValue: ''
+  })));
+  setState(new Map(state.set('phone', {
+    inputName: 'phone',
+    label: 'Phone',
+    inputRef: useRef(null),
+    inputValue: ''
+  })));
+  setState(new Map(state.set('company', {
+    inputName: 'company',
+    label: 'Company name',
+    inputRef: useRef(null),
+    inputValue: ''
+  })));  // End setup of useState
+
   const handleChange = (e) => {
+    /*
     setState({
       [e.target.name]: e.target.value
-    });  // End call to setState()
+    }
+    */
+    let currentInputValue = state.get(e.target.name);
+    currentInputValue.inputValue = e.target.value;
+    setState(new Map(state.set(e.target.name, currentInputValue)));
   };  // End handleChange function
-
+  const renderForm = () => {
   return (
-    <div className="pam">
-      <h2>Multiple Errors</h2>
-      <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
+      state.values().forEach(element => {      
         <div className="slds-form-element" id="form-input-1">
           <label className="slds-form-element__label" htmlFor="name-input-1">
-            <abbr className="slds-required" title="required">*</abbr>First name
+            <abbr className="slds-required" title="required">*</abbr>
+            {element.label}
           </label>
           <div className="slds-form-element__control">
             <input 
               aria-describedby={describedbyValue()}
-                className="slds-input" 
-                id="name-input-1" 
-                required={true} 
-                type="text" 
+              className="slds-input" 
+              id="name-input-1" 
+              required={true} 
+              type="text" 
+              ref={inputRef}
+              name={element.inputName}
             />
           </div>
           <div className="slds-form-element__help" id="input-error-1" role={renderRole()} style={{display: "none"}}>Complete this field</div>
         </div>
-      </form>
+      });  // End forEach 
+    </form>
+  )
+  };  // End renderForm function
+
+  return (
+    <div className="pam">
+      <h2>Multiple Errors</h2>
     </div>
   );
 }; // End multipleErrors component
-export default multipleErrors;
+export default MultipleErrors;
