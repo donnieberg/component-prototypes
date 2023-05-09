0b31d973ae1836feb394fad0762ad4c86b6f526c
Adam Rodenbeck <arodenbeck@salesforce.com>
2023-05-09 11:29:33 -0400

    Add multipleErrorsPage. Still need to update it to call teh MutlipleErrors component and update all ereferences to errorMessaging.

---
+++ src/pages/multipleErrorsPage.js
@@ -0,0 +1,26 @@
+import Panel from './panel.js'
+import MultipleErrors from '../multipleErrors/multipleErrors.js'
+import { useOutletContext } from 'react-router-dom';
+import { useState } from 'react';
+import { Button, Checkbox, RadioButtonGroup, Radio } from '@salesforce/design-system-react';
+
+const MultipleErrorsPage = () => {
+    const [panelOpen, handleSettingsClick] = useOutletContext();
+    let [ariaLive, setAriaLive] = useState('none');
+    let [hasError, setHasError] = useState(false);
+
+    const settings = [
+        { id: 'inputError', labels: { label: 'Markup for Input Errors'}, options: ['none', 'status', 'alert'], }
+    ];
+
+    return (
+        <div className="df df-spaceBetween">
+            <section>
+                <ErrorMessaging ariaLive={ariaLive} hasError={hasError} setHasError={setHasError} />
+            </section>
+            {panelOpen ? <Panel settings={settings} optionValue={ariaLive} setOptionValue={setAriaLive} /> : null }
+        </div>
+    );
+};
+
+export default MultipleErrorsPage;
